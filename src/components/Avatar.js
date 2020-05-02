4561fa2fdbddb7a8235b045bbbaa39146297558a
Toseben <arekoskela@gmail.com>
2020-05-02 20:25:24 +0300

    Circle of avatars with different offsets

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -5,44 +5,18 @@ import { useSpring, a } from 'react-spring/three'
 import * as easings from 'd3-ease'
 import KeyMaterial from "../shaders/KeyMaterial"
 
-export default function Avatar({ useStore }) {
+export default function Avatar({ texture, pos }) {
   const meshRef = useRef();
-  const videoRef = useRef();
   const { camera } = useThree()
 
-  useEffect(() => {
-    if (true) {
-      meshRef.current.visible = true
-      if (videoRef.current) videoRef.current.play()
-    }
-    else {
-      meshRef.current.visible = false
-      if (videoRef.current) videoRef.current.pause()
-      if (videoRef.current) videoRef.current.currentTime = 0;
-    }
-  }, [])
-
-  const receptionistTex = useMemo(() => {
-    const video = document.createElement('video');
-    video.src = "assets/avatar.mp4";
-    video.loop = true
-    video.muted = true
-    video.load();
-    videoRef.current = video
-
-    const texture = new THREE.VideoTexture(video);
-    texture.minFilter = THREE.LinearFilter;
-    texture.magFilter = THREE.LinearFilter;
-    texture.format = THREE.RGBFormat;
-    return texture
-  }, [])
-
-  const keyMaterial = React.useMemo(() => {
-    receptionistTex.encoding = THREE.sRGBEncoding;
-    return new KeyMaterial({
-      texture0: receptionistTex,
-      lum: new THREE.Vector2(1, 1)
+  const keyMaterial = useMemo(() => {
+    return new THREE.MeshBasicMaterial({
+      map: texture
     })
+    // return new KeyMaterial({
+    //   texture0: avatarTex,
+    //   lum: new THREE.Vector2(1, 1)
+    // })
   }, [])
 
   useEffect(() => {
@@ -64,7 +38,7 @@ export default function Avatar({ useStore }) {
     <>
       <mesh
         ref={meshRef}
-        position={[0, 0, 2]}>
+        position={pos}>
         <planeGeometry attach="geometry" args={[280 * scale, 480 * scale]} />
         <meshLambertMaterial attach="material" side={THREE.DoubleSide} />
       </mesh>
--- src/components/Graphics.js
+++ src/components/Graphics.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three'
-import React, { Suspense, useRef } from 'react'
+import React, { Suspense, useRef, useMemo, useEffect } from 'react'
 import { Canvas, extend, useThree, useFrame } from 'react-three-fiber';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { useLoader } from "react-three-fiber"
@@ -17,13 +17,60 @@ function ControlsOrbit() {
   )
 }
 
+function Avatars() {
+  const avatarArray = new Array(50).fill(null)
+  const radius = 3
+
+  const videoArray = useMemo(() => {
+    let videoArray = new Array(5).fill(null)
+    return videoArray.map((node, idx) => {
+      const video = document.createElement('video');
+      video.src = `assets/avatar.mp4`;
+      video.loop = true
+      video.muted = true
+      video.id = `video-${idx}`
+      video.load();
+
+      // document.body.append(video)
+      video.currentTime = idx / videoArray.length * 1.8
+      video.play()
+
+      const texture = new THREE.VideoTexture(video);
+      texture.minFilter = THREE.LinearFilter;
+      texture.magFilter = THREE.LinearFilter;
+      texture.format = THREE.RGBFormat;
+      texture.encoding = THREE.sRGBEncoding;
+
+      return { video, texture }
+    })
+  }, [])
+
+  // useEffect(() => {
+  //   return () => {
+  //     videoArray.forEach(vid => {
+  //       if (vid['video'].parentNode) document.body.removeChild(vid['video'])
+  //     })
+  //   }
+  // }, [])
+
+  return (
+    <group>
+      {avatarArray.map((node, idx) => {
+        const x = Math.sin(idx / avatarArray.length * Math.PI * 2) * radius
+        const z = Math.cos(idx / avatarArray.length * Math.PI * 2) * radius
+        return <Avatar key={idx} texture={videoArray[idx % videoArray.length].texture} pos={new THREE.Vector3(x, 0, z)} />
+      })}
+    </group>
+  )
+}
+
 const Graphics = ({ }) => {
   return (
     <Canvas
       gl={{ antialias: true }}
       onCreated={({ gl }) => {
-        gl.setClearColor(0xBDCAC0)
-        gl.toneMapping = THREE.ACESFilmicToneMapping
+        // gl.setClearColor(0xBDCAC0)
+        // gl.toneMapping = THREE.ACESFilmicToneMapping
         gl.outputEncoding = THREE.sRGBEncoding
       }}
       camera={{
@@ -31,9 +78,10 @@ const Graphics = ({ }) => {
         position: new THREE.Vector3(5, 2.5, 5)
       }}>
 
+      <ambientLight />
       <ControlsOrbit />
       <Suspense fallback={null}>
-        <Avatar />
+        <Avatars />
       </Suspense>
     </Canvas>
   );
